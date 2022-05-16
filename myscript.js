3e565325db32a9cb55a3da97d0d440a51d9ddf86
Griffin <griffinquarles11@gmail.com>
2022-05-16 14:10:51 -0500

    UI Update for RPS

--- myscript.js
+++ myscript.js
@@ -1,68 +1,88 @@
 let playerScore = 0;
 let compScore = 0;
+let rockBtn = document.getElementById('rock');
+let paperBtn = document.getElementById('paper');
+let scissorsBtn = document.getElementById('scissors');
+let playerPick = "";
+const results = document.createElement('div');
+results.classList.add('results');
+document.body.appendChild(results);
+
 
 
 function computerPlay(){
     const choice = ["Rock","Paper","Scissors"];
     let computerChoice = choice[Math.floor(Math.random() * choice.length)];
     return computerChoice;
+}
+
+
+function playerChoice(){
+    if(document.getElementById('rock').clicked == true){
+        return playerPick = 'rock';
+    }
+    else if(document.getElementById('paper').clicked == true){
+        return playerPick ='paper';
+    }
+    else if(document.getElementById('scissors').clicked == true){
+        return playerPick = 'scissors';
+    }
+    
+    
+    
     
 }
 
-function playRound(playerSelection, computerSelection){
-    let playerPick = prompt("Do you choose rock, paper, or scissors?");
+
+
+
+
+function playRound(val){
+    
+
+    let playerSelection = val;
+    console.log("Player Chose: " + playerSelection);
     let computerPick = computerPlay();
     //Prints what the computer chose
     console.log("The computer chose: " + computerPick);
-    if (playerPick.toLowerCase() == "rock" && computerPick == "Rock"){
-        return("It is a tie! Computer score is: "+compScore + " Player Score is: " +playerScore);
+    if (playerSelection == "rock" && computerPick == "Rock"){
+        results.textContent = ("It is a tie! Computer score is: "+ compScore + " Player Score is: " + playerScore);
     }
-    else if (playerPick.toLowerCase() == "rock" && computerPick == "Paper"){
-        return("Computer wins! Computers score is: " + (compScore += 1));
+    else if (playerSelection == "rock" && computerPick == "Paper"){
+        results.textContent = ("Computer wins! Computers score is: " + (compScore += 1) + " Player Score is: " + playerScore);
     }
-    else if(playerPick.toLowerCase() == "rock" && computerPick == "Scissors"){
-        return("Player wins! Player score is: " + (playerScore +=1));
+    else if(playerSelection == "rock" && computerPick == "Scissors"){
+        results.textContent =("Player wins! Player score is: " + (playerScore +=1) + " Computer score is: "+compScore);
     }
-    else if(playerPick.toLowerCase() == "paper" && computerPick == "Rock"){
-        return("Player wins! Player score is: " + (playerScore +=1));
+    else if(playerSelection == "paper" && computerPick == "Rock"){
+        results.textContent = ("Player wins! Player score is: " + (playerScore +=1)+ " Computer score is: "+compScore);
     }
-    else if(playerPick.toLowerCase() == "paper" && computerPick == "Paper"){
-        return("It is a tie. Computer score is:"+compScore + "Player Score is: " +playerScore);
+    else if(playerSelection == "paper" && computerPick == "Paper"){
+        results.textContent = ("It is a tie. Computer score is: "+compScore + " Player Score is: " +playerScore);
     }
-    else if(playerPick.toLowerCase() == "paper" && computerPick == "Scissors"){
-        return("Computer Wins! Computers score is: "+ (compScore += 1));
+    else if(playerSelection == "paper" && computerPick == "Scissors"){
+        results.textContent = ("Computer Wins! Computers score is: "+ (compScore += 1) + " Player Score is: " + playerScore);
     }
-    else if(playerPick.toLowerCase() == "scissors" && computerPick == "Rock"){
-        return("Computer Wins! Computers score is: " + (compScore += 1));
+    else if(playerSelection == "scissors" && computerPick == "Rock"){
+        results.textContent = ("Computer Wins! Computers score is: " + (compScore += 1) + " Player Score is: " + playerScore);
     }
-    else if(playerPick.toLowerCase() == "scissors" && computerPick == "Paper"){
-        return("Player wins! Player score is: " + (playerScore +=1));
+    else if(playerSelection == "scissors" && computerPick == "Paper"){
+        results.textContent = ("Player wins! Player score is: " + (playerScore +=1)+ " Computer score is: "+compScore);
     }
-    else if(playerPick.toLowerCase() == "scissors" && computerPick == "Scissors"){
-        return("It is a tie.Computer score is:"+compScore + "Player Score is: " +playerScore);
+    else if(playerSelection == "scissors" && computerPick == "Scissors"){
+        results.textContent = ("It is a tie. Computer score is: "+compScore + " Player Score is: " +playerScore);
+    }
+
+    if(compScore == 5){
+        results.textContent = "Game over! Computer Wins! Comp Score: " + compScore + " Player Score: " + playerScore;
+        compScore = 0;
+        playerScore = 0;
+    }
+    else if(playerScore == 5){
+        results.textContent = "Game over! Player Wins! Player Score: " + playerScore + " Computer Score: " + compScore;
+        playerScore = 0;
+        compScore = 0;
     }
 }
 
 
-
-
-function game(){
-    for (let i = 0; i < 5; i++){
-        
-        
-        console.log(playRound());
-
-        if(i == 4 && playerScore > compScore){
-            alert("Game Over Player Wins!  Player Score: " + playerScore + " Computer Score: "+ compScore);
-        }
-        else if(i == 4 && playerScore < compScore){
-            alert("Game Over Computer Wins! Player Score: " + playerScore + " Computer Score: "+ compScore);
-        }
-        else if(i == 4 && playerScore == compScore){
-            alert("Game Over It is a Tie! Player Score: " + playerScore + " Computer Score: "+ compScore);
-        }
-
-    }
-}
-
-game();
